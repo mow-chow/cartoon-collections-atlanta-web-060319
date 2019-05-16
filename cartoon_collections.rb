5a0dc1584e488c9c547c331b8e9a0bbb43f756f2
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-16 16:10:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,7 @@ def summon_captain_planet(planeteer_calls)
   summon_captain_planet
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
 end
 
 def find_the_cheese# code an argument here
